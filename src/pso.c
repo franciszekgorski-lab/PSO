54536c12549a6df0a880bcaebe44d904b6704b6c
Me54nger <grzadka.artur@gmail.com>
2026-01-28 22:54:05 +0100

    pso
    naprawa literowek i prostych bledow

--- src/pso.c
+++ src/pso.c
@@ -3,27 +3,28 @@
 #include <stdlib.h>
 
 swarm* swarm_construct(int particle_number, Map * map){
-    srand(time);
+    srand(time(NULL));
     swarm * s = malloc(sizeof(swarm*));
     s->size = particle_number;
-    s->lowest_found = 0; /// TU TRZEBA ZMNIENIC NA NAJWIEKSZA MOZLIWA PRZY GENERACJI WARTOSC
+    s->lowest_found_x = 0;
+    s->lowest_found_y = 0;
     s->particle_arr = malloc(sizeof(particle*)*particle_number);
 
     for(int i =0; i<particle_number; i++){
         s->particle_arr[i] = malloc(sizeof(particle));
         s->particle_arr[i]->pos_x = rand()%(map->width);
-        s->particle_arr[i]->pos_x = rand()%(map->height);
+        s->particle_arr[i]->pos_x = rand()%(map->heigth);
         s->particle_arr[i]->vel_x = 0;
         s->particle_arr[i]->vel_y = 0;
-        s->particle_arr[i]->lowest_found = 0; // TU TRZEBA ZMNIENIC NA NAJWIEKSZA MOZLIWA PRZY GENERACJI WARTOSC
+        s->particle_arr[i]->lowest_found_x = 0;
+        s->particle_arr[i]->lowest_found_y = 0;
     }
     return s;
 }
-// return 1 - jeszcze jakies czastki pozostaja w ruchu
 // TU JAKO ARGUMENTY TRZEBA DODAC WAGI I WPISAC JE W ROWNANIA
-int update_particles(swarm * s, Map *m){
+void update_particles(swarm * s, Map *m){
     //dla każdej cząstki
-    for(int i =0; i<particle_number; i++){
+    for(int i =0; i<s->size; i++){
         particle * p =  s->particle_arr[i];
         //zmieniamy pozycje o predkosc
         p->pos_x += p->vel_x;
@@ -45,16 +46,16 @@ int update_particles(swarm * s, Map *m){
         //losujemy prędkość - składowa x
         float r1 = (float)rand()/(float)(RAND_MAX);
         float r2 = (float)rand()/(float)(RAND_MAX);
-        p->vel_x = 1 * vel_x + r1 * 1 * (p->lowest_found_x - p->pos_x) + r2 * 1 * (s->lowest_found_x - p->pos_x);
+        p->vel_x = 1 * p->vel_x + r1 * 1 * (p->lowest_found_x - p->pos_x) + r2 * 1 * (s->lowest_found_x - p->pos_x);
         //jesli spowoduje ona, ze czastka wychodzi za mape to zmieniamy kierunek na przeciwny
         if(p->pos_x + p->vel_x > m->width){
             p->vel_x *= -1;
         }
         r1 = (float)rand()/(float)(RAND_MAX);
         r2 = (float)rand()/(float)(RAND_MAX);
-        p->vel_y = 1*vel_y + r1 * 1 * (p->lowest_found_y - p->pos_y) + r2 * 1 * (s->lowest_found_y - p->pos_y);
+        p->vel_y = 1*p->vel_y + r1 * 1 * (p->lowest_found_y - p->pos_y) + r2 * 1 * (s->lowest_found_y - p->pos_y);
         //to samo dla skladowej y
-        if(p->pos_y + p->vel_y > m->height){
+        if(p->pos_y + p->vel_y > m->heigth){
             p->vel_y *= -1;
         }
     }
@@ -62,7 +63,7 @@ int update_particles(swarm * s, Map *m){
 
 void destroy_swarm(swarm * s){
     for(int i = 0; i< s->size; i++){
-        free(particle_arr[i]);
+        free(s->particle_arr[i]);
     }
     free(s->particle_arr);
     free(s);
