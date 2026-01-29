13f51c186442fdf9038a4b3f1d1c182bffbbe145
Me54nger <grzadka.artur@gmail.com>
2026-01-29 13:46:14 +0100

    komentarze pso.c

--- src/pso.c
+++ src/pso.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//czyta z pliku 3 liczby ktore sa odpowiednimi argumentami dla pso - w,c1,c2
 pso_settings read_pso_settings(const char* file_path){
     pso_settings ret =(pso_settings){1,1,1};
     FILE* fptr = fopen(file_path, "r");
     if (fptr == NULL) {
             printf("Błąd przy czytaniu mapy z pliku!\n");
-            return (pso_settings){1,1,1};
+            return (pso_settings){1,1,1}; //zwraca domyslna wartosc
     }
 
     fscanf(fptr,"%lf %lf %lf ", &ret.w, &ret.c1, &ret.c2);
@@ -16,16 +17,20 @@ pso_settings read_pso_settings(const char* file_path){
     return ret;
 }
 
+//incjalizuje roj czastek
 swarm* swarm_construct(int particle_number, Map * map){
+    //seeduje random
     srand(time(NULL));
     swarm * s = malloc(sizeof(swarm));
+    //inicjalizuje pola structa
     s->size = particle_number;
     s->highest_found_x = 0;
     s->highest_found_y = 0;
     s->particle_arr = malloc(sizeof(particle*)*particle_number);
+    //inicjalizuje pola structa kazdej czastki
     for(int i =0; i<particle_number; i++){
         particle * p = malloc(sizeof(particle));
-
+        
         p->pos_x = rand()%(map->width);
         p->pos_y = rand()%(map->heigth);
         p->vel_x = 0;
@@ -33,6 +38,7 @@ swarm* swarm_construct(int particle_number, Map * map){
         p->highest_found_x = (int)(p->pos_x);
         p->highest_found_y = (int)(p->pos_y);
 
+        //szuka najwyzszego pola ze wszystkich w ktorych wylosowały się cząstki
         int ind = (int)p->pos_x + (int)p->pos_y * map->width;
         int highest_ind = s->highest_found_x + s->highest_found_y * map->width;
         if(map->depth->value[ind] > map->depth->value[highest_ind]){
@@ -44,7 +50,7 @@ swarm* swarm_construct(int particle_number, Map * map){
     }
     return s;
 }
-// TU JAKO ARGUMENTY TRZEBA DODAC WAGI I WPISAC JE W ROWNANIA
+// funkcja odpowiada za aktualizowanie pozycji i predkosci kazdej czastki
 void update_particles(swarm * s, Map *m, pso_settings pso_s){
     //dla każdej cząstki
     for(int i =0; i<s->size; i++){
@@ -57,21 +63,20 @@ void update_particles(swarm * s, Map *m, pso_settings pso_s){
         if((p->pos_x + p->vel_x > m->width) || (p->pos_x + p->vel_x < 0)){
             p->vel_x *= -1;
         }
+        //losujemy predkosc - skladowa y
         r1 = (float)rand()/(float)(RAND_MAX);
         r2 = (float)rand()/(float)(RAND_MAX);
         p->vel_y = pso_s.w * p->vel_y + r1 * pso_s.c1 * (p->highest_found_y - p->pos_y) + r2 * pso_s.c2 * (s->highest_found_y - p->pos_y);
-        //to samo dla skladowej y
+        //jesli spowoduje ona, ze czastka wychodzi za mape to zmieniamy kierunek na przeciwny
         if((p->pos_y + p->vel_y > m->heigth) || (p->pos_y + p->vel_y < 0)){
             p->vel_y *= -1;
         }
-
-        printf(" PREDKOSC %lf %lf \n", p->vel_x, p->vel_y);
 
         //zmieniamy pozycje o predkosc
         p->pos_x += p->vel_x;
         p->pos_y += p->vel_y;
 
-                int ind = (int)p->pos_x + (int)p->pos_y * m->width;
+        int ind = (int)p->pos_x + (int)p->pos_y * m->width;
         int highest_ind = p->highest_found_x + p->highest_found_y * m->width;
         //sprawdzamy czy nowa pozycja to jest jakieś minimum
         // tutaj lokalne dla cząstki
@@ -87,11 +92,12 @@ void update_particles(swarm * s, Map *m, pso_settings pso_s){
         }
     }
 }
-
+//freeuje wszystko
 void destroy_swarm(swarm * s){
     for(int i = 0; i< s->size; i++){
         free(s->particle_arr[i]);
     }
     free(s->particle_arr);
     free(s);
+
 }
