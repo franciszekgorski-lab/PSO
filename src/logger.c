eb54a9dccb4434566f004d4bcf76eafde8abd8f9
Me54nger <grzadka.artur@gmail.com>
2026-01-29 13:36:12 +0100

    logger.c komentarze

--- src/logger.c
+++ src/logger.c
@@ -1,8 +1,11 @@
 #include "logger.h"
 
+//wypisuje gorny rzad pliku csv z nazwami kolumn
 void log_headers(FILE * fptr, swarm * s){
+        //1 kolumna to numer - ktory mowi nam ile razy pozycje byly dotad zloggowane
         fprintf(fptr, "numer log,");
         for(int j=0; j<s->size; j++){
+                //nastepne 3 kolumny dla każdej komorki to jej numer, i pozycja x i y
                 fprintf(fptr, "nr.czastki,X,Y,");
         }
         fprintf(fptr, "\n");
@@ -18,6 +21,7 @@ void log_positions( FILE * fptr, swarm * s, int iteration){
     fprintf(fptr,"\n");
 }
 
+//zapisuje rezultaty tzn pozycje x i y (calkowita) najwyzszego znalezionego punktu
 void log_results(FILE * fptr, swarm * s){
     fprintf(fptr,"%d,%d",  s->highest_found_x,s->highest_found_y);
 }
